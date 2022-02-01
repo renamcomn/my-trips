7faf97868afc529540d1f0c841a2a8e523b6ca72
renamcomn <acidmud@gmail.com>
2022-02-01 18:08:59 -0300

    add place detail styled

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,4 +1,9 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+import LinkWrapper from 'components/LinkWrapper'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+
+import * as S from './styles'
 
 type ImageProps = {
   url: string
@@ -10,7 +15,7 @@ export type PlaceTemplateProps = {
   place: {
     slug: string
     name: string
-    description: {
+    description?: {
       html: string
     }
     gallery: ImageProps[]
@@ -18,20 +23,37 @@ export type PlaceTemplateProps = {
 }
 
 export default function PlaceTemplate({ place }: PlaceTemplateProps) {
+  const router = useRouter()
+
+  if (router.isFallback) return null
+
   return (
     <>
-      <h1>{place.name}</h1>
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }}></div>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Go back to Map" />
+      </LinkWrapper>
 
-      {place.gallery.map((image, index) => (
-        <Image
-          key={`photo-${index}`}
-          src={image.url}
-          alt={place.name}
-          width={image.width}
-          height={image.height}
-        />
-      ))}
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description?.html || '' }}
+          ></S.Body>
+
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <Image
+                key={`photo-${index}`}
+                src={image.url}
+                alt={place.name}
+                width={1000}
+                height={600}
+                quality={75}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
     </>
   )
 }
